2423817468f2d2aca1d021d4d7dd3c680220d3ce
Sergei Matiushenko <csserezha@gmail.com>
2021-08-23 22:56:17 +0300

    build: rebuild packages

--- lib/esm/components/monthView.d.ts
+++ lib/esm/components/monthView.d.ts
@@ -1,4 +1,4 @@
 import * as React from 'react';
-import { TMonthViewProps } from '../types';
+import { TMonthViewProps } from 'types';
 declare const _default: React.MemoExoticComponent<({ data, activeView, firstDayIsMonday, onClick, }: TMonthViewProps) => JSX.Element | null>;
 export default _default;
--- lib/esm/hooks/useCalendar.d.ts
+++ lib/esm/hooks/useCalendar.d.ts
@@ -1,3 +1,3 @@
-import { TSelectedDay, TUseCalendarReturn } from '../types';
+import { TSelectedDay, TUseCalendarReturn } from 'types';
 export declare const useCalendar: (selectedDay: TSelectedDay | undefined, markers: number[], firstDayIsMonday?: boolean) => TUseCalendarReturn;
 export default useCalendar;
