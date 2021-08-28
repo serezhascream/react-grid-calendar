7e37b24bab48c453446ad0f5f1fa4bbb771f15d5
Sergei Matiushenko <csserezha@gmail.com>
2021-08-28 15:46:49 +0300

    fix: tests for the WeekdayTitles component

--- src/tests/components/weekdayTitles.test.tsx
+++ src/tests/components/weekdayTitles.test.tsx
@@ -2,16 +2,18 @@ import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import WeekdayTitles from '../../lib/components/weekdayTitles';
+import { getLocalizedNames } from '../../lib/utils/localization';
 import { WEEKDAY_TITLES } from '../../lib/data/constants';
 import { weekdayTitlesRus } from '../fixture';
 import { testIds } from '../../lib/data/tests';
-import { TWeekdayTitles } from '../../lib/types';
 
 describe('components > WeekdayTitles', () => {
+	const { weekdays: defaultWeekdayTitles } = getLocalizedNames({ locale: 'en-US', firstDayIsMonday: true });
+	
 	it('renders', () => {
 		render(
 			<WeekdayTitles
-				weekdayTitles={WEEKDAY_TITLES as TWeekdayTitles}
+				weekdayTitles={WEEKDAY_TITLES}
 				firstDayIsMonday
 			/>
 		);
@@ -24,20 +26,22 @@ describe('components > WeekdayTitles', () => {
 	it('renders correctly when week starts on monday', () => {
 		render(
 			<WeekdayTitles
-				weekdayTitles={WEEKDAY_TITLES as TWeekdayTitles}
+				weekdayTitles={defaultWeekdayTitles}
 				firstDayIsMonday
 			/>
 		);
 
 		const titles = screen.getByTestId(testIds.weekdayTitles);
 
-		expect(titles.children[0].textContent).toEqual('mon');
+		expect(titles.children[0]).toHaveTextContent('Mon');
 	});
 	
 	it('renders correctly when week starts on sunday', () => {
+		const { weekdays } = getLocalizedNames({ locale: 'en-US', firstDayIsMonday: false });
+		
 		render(
 			<WeekdayTitles
-				weekdayTitles={WEEKDAY_TITLES as TWeekdayTitles}
+				weekdayTitles={weekdays}
 				firstDayIsMonday={false}
 			/>
 		);
@@ -45,24 +49,20 @@ describe('components > WeekdayTitles', () => {
 
 		const titles = screen.getByTestId(testIds.weekdayTitles);
 
-		expect(titles.children[0].textContent).toEqual('sun');
+		expect(titles.children[0]).toHaveTextContent('Sun');
 	});
-	it('renders correctly with provided weekday titles', () => {
-		let weekdays = [];
+	it('renders correctly with russian locale', () => {
+		const { weekdays } = getLocalizedNames({ locale: 'ru-RU', firstDayIsMonday: true });
 		
 		render(
 			<WeekdayTitles
-				weekdayTitles={weekdayTitlesRus as TWeekdayTitles}
+				weekdayTitles={weekdays}
 				firstDayIsMonday
 			/>
 		);
 
 		const titles = screen.getByTestId(testIds.weekdayTitles);
 		
-		for (let i = 0; i < titles.children.length; i++) {
-			weekdays.push(titles.children[i].textContent);
-		}
-		
-		expect(weekdays).toEqual(weekdayTitlesRus);
+		expect(titles.children[0]).toHaveTextContent('Пн');
 	});
 });
