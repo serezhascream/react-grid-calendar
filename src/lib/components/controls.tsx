17f1b09411b2c8ecc9d3d049073d41afe10d03c5
Sergei Matiushenko <csserezha@gmail.com>
2021-08-28 22:16:42 +0300

    refactor: remove classnames

--- src/lib/components/controls.tsx
+++ src/lib/components/controls.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import classNames from 'classnames';
 
 import { TSwitchDirection, TYearAndMonth } from '../types';
 import { testIds } from '../data/tests';
--- src/lib/components/day.tsx
+++ src/lib/components/day.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import classNames from 'classnames';
 
 import { TDayObject, TDaySelectFunc } from '../types';
 import { testIds } from '../data/tests';
