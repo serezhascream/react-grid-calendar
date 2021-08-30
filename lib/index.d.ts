78ece6c6d31a48b4381c650d4ee4d0084412d2d3
Sergei Matiushenko <csserezha@gmail.com>
2021-08-30 11:31:14 +0300

    build: rebuild project

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { TDayObject } from './types';
 import './styles/index.scss';
 interface Props {
     firstDayIsMonday?: boolean;
@@ -7,7 +6,7 @@ interface Props {
     markers?: number[];
     locale?: string;
     classPrefix?: string | string[] | null;
-    onSelectDay?: (day: TDayObject) => void;
+    onSelectDay?: (day: Date | null) => void;
 }
 declare const _default: React.NamedExoticComponent<Props>;
 export default _default;
