2a3142c8a5889c75ff238becc91c97a362b815da
Sergei Matiushenko <csserezha@gmail.com>
2021-09-02 08:12:05 +0300

    build: remove test build directory

--- lib/components/controls/arrowButton.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-interface Props {
-    direction: 'prev' | 'next';
-    classPrefix?: string | string[] | null;
-    activeView: string;
-    onClick: (direction: string) => void;
-}
-declare const _default: React.NamedExoticComponent<Props>;
-export default _default;
--- lib/hooks/useDecade.d.ts
+++
@@ -1,8 +0,0 @@
-import { TSwitchDirection } from '../types';
-interface TReturn {
-    decade: number[];
-    switchDecade: TSwitchDirection;
-}
-export declare const getDecade: (year: number) => number[];
-export declare const useDecade: (currentYear: number) => TReturn;
-export default useDecade;
